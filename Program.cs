f4301ba9fbf1e8ff6b4c9f3460d37d313cbcd08f
Tinashe Chitakunye <tinashec93@live.com>
2022-12-06 11:56:42 +0200

    chore: continuued q&a

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
-﻿var nameMaker = new[] {'t','i','n','a','s','h','e'};
+﻿using System.Runtime.InteropServices;
 
-string fullname = new string(nameMaker);
-Console.Write(fullname.GetType().GetType());
+var myOrders = new Order[]
+{
+    new Order("sadza and beef stew", 3, 80),
+    new Order("rice and beef stew", 2, 85),
+    new Order("chips and chicken", 1, 43),
+    new Order("rice and truffles", 1, 80),
+    new Order("spaghetti and white sauce", 4, 70),
+    new Order("samp and turkey", 2, 64)
+};
+
+var ordersSpan = CollectionsMarshal.AsSpan<Order>();
+
+var lessThan2Orders = from orderItem in myOrders
+                      where orderItem.quantity >= 2
+                      orderby orderItem.orderName
+                      select orderItem;
+
+foreach (var item in lessThan2Orders)
+{
+    Console.WriteLine(item);
+}
+
+
+record Order(string orderName, byte quantity, float nutValue);
